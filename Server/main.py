6338d52f384a58615ffeaea722395a6c55c19ed3
Cole Arduser <cole-arduser@uiowa.edu>
2023-09-11 11:37:47 -0500

    edit main and api files

--- Server/main.py
+++ Server/main.py
@@ -1,21 +1,76 @@
-from fastapi import FastAPI
+import time
+from datetime import datetime
 
-app = FastAPI()
-
-
-@app.get("/past")
-async def past():
-    # get past 300 sec of data and return it
-    pass
+import RPi.GPIO as GPIO
+import apis
 
 
-@app.get("/current")
-async def current():
-    # get current data and return it
-    pass
+class Pi:
+    def __init__(self):
+        self.temp_data = []
+        self.switch_status = True
+        self.button_status = False
+        self.button_pin = 17
+        self.sensor_id = '28-3ce0e381d163'
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(self.button_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+        GPIO.add_event_detect(self.button_pin, GPIO.FALLING, callback=self.button_interrupt, bouncetime=200)
+
+    def button_interrupt(self, channel):
+        print("Button pressed!")
+
+        # Wait for the button to be released
+        while GPIO.input(self.button_pin) == GPIO.LOW:
+            # Push the Temperatures to the LCD
+            print("Current Temperature: " + str(self.temp_data[len(self.temp_data) - 1]))
+            pass
+
+        print("Button released!")
+        # Put your interrupt function code here...
+
+    def read_temperature(self):
+        try:
+            # Read the raw temperature data from the sensor
+            with open(f"/sys/bus/w1/devices/{self.sensor_id}/w1_slave", "r") as sensor_file:
+                lines = sensor_file.readlines()
+
+            # Extract the temperature from the second line of the output
+            temperature_line = lines[1].strip().split("=")[-1]
+            temperature_celsius = float(temperature_line) / 1000.0
+
+            return temperature_celsius
+
+        except Exception as e:
+            print(f"An error occurred: {str(e)}")
+            return None
+
+    def temp_loop(self):
+        start_time = datetime.now()
+        temperature = self.read_temperature()
+
+        if len(self.temp_data) >= 300:
+            self.temp_data.pop(0)
+
+        if temperature is None:
+            print("ERROR")
+        elif temperature == 0.0:
+            self.temp_data.append(False)
+        else:
+            self.temp_data.append(temperature)
+
+        # print("Temperature: " + str(tempValues) + " °C\n")
+        print("Length of Queue: " + str(len(self.temp_data)))
+        sleep_time = datetime.now() - start_time
+        print(sleep_time)
+        time.sleep(sleep_time.total_seconds())
 
 
-@app.post("/activate/{status}")
-async def activate():
-    # set the LCD to the input status (bool)
-    pass
+def main():
+    pi = Pi()
+    apis.start_server()
+    while True:
+        pi.temp_loop()
+
+
+if __name__ == "__main__":
+    main()
